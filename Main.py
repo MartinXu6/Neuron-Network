c83db94b57d6a5b33b83d009dcc33acb1c9c51af
MartinXu <20XuPM@tonbridge-school.org>
2024-01-17 11:23:27 +0000

    created activation functions

--- Main.py
+++ Main.py
@@ -22,11 +22,12 @@ class Neuron_layer:
     def __init__(self):
         pass
 
-    def Softmax(self,outputs):
-        divider = sum([math.e**i for i in outputs])
-        return [math.e**i/divider for i in outputs]
 
-    
-n1 = Neuron_layer()
-print(n1.Softmax([1.3,5.1,2.2,0.7,1.1]))
+class out_layer(Neuron_layer):
+    def Softmax(self, outputs):
+        divider = sum([math.e ** i for i in outputs])
+        return [math.e ** i / divider for i in outputs]
 
+
+n1 = out_layer()
+print(n1.Softmax([1.3, 5.1, 2.2, 0.7, 1.1]))
