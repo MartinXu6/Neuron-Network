08ae6c526a71b2eca3c4cd854f7a8565df2fd792
MartinXu <20XuPM@tonbridge-school.org>
2024-01-29 20:19:11 +0000

    put the initialisation of the neuron layers into a big class, and stored all the weights in a massive 3D matrix, so that they can be accessed and changed with ease

--- Main.py
+++ Main.py
@@ -9,6 +9,7 @@ image = Image.open(r"D:\pycharm projects\Neuron-Network\images\cat1.jpg")
 label = [0, 1]
 image.thumbnail((200, 200))
 pixels = image.load()
+# extracting pixels into tuples of RGB values, and separate each pixel into three input neurons in the neuron network
 input_pixels = []
 for row in range(image.size[0]):
     for col in range(image.size[1]):
@@ -23,7 +24,8 @@ weight_matrix = [[[random.uniform(-0.5, 0.5) for j in range(len(input_pixels))] 
                  [[random.uniform(-0.5, 0.5) for j in range(50)] for i in
                   range(50)],
                  [[random.uniform(-0.5, 0.5) for j in range(50)] for i in
-                  range(2)]]
+                  range(2)]
+                 ]
 
 
 class Neuron:
@@ -31,13 +33,10 @@ class Neuron:
         self.connected = connected
         self.weights = weights
         self.bias = random.uniform(-1, 1)
+        self.activation = self.Sigmoid(np.dot(self.connected, self.weights))
 
     def Sigmoid(self, value):
         return 1 / (1 + math.e ** (value * -1))
-
-    def forward_pass(self):
-        num = np.dot(self.connected, self.weights)
-        return self.Sigmoid(num)
 
 
 class Neuron_layer:
@@ -53,11 +52,11 @@ class Neuron_layer:
         return [math.e ** i / divider for i in outputs]
 
     def get_output(self):
-        output_neurons = [neuron.forward_pass() for neuron in self.neurons]
+        output_neurons = [neuron.activation for neuron in self.neurons]
         return output_neurons
 
     def get_prediction(self):
-        output_neurons = [neuron.forward_pass() for neuron in self.neurons]
+        output_neurons = [neuron.activation for neuron in self.neurons]
         return self.Softmax(output_neurons)
 
 
@@ -68,15 +67,13 @@ class Neural_Network:
         self.Hidden1 = Neuron_layer(self.input_layer.get_output(), 50, weight_matrix[1])
         self.Hidden2 = Neuron_layer(self.Hidden1.get_output(), 50, weight_matrix[2])
         self.Hidden3 = Neuron_layer(self.Hidden2.get_output(), 2, weight_matrix[3])
-        self.predictions = self.Hidden3.get_output()
+        self.out = self.Hidden3.get_output()
 
 
-# extracting pixels into tuples of RGB values, and separate each pixel into three input neurons in the neuron network
-
-# input_neurons = len(input_pixels)
 # network initialisation
 Network = Neural_Network(input_pixels)
-print(Network.predictions)
+predictions = Network.out
+print(predictions)
 
 
 # training
@@ -87,5 +84,11 @@ def dcdw(y, y_hat, z, a):
     return dc_dyhat * dyhat_dz * dzdw
 
 
-def back_propagation():
-    return
+def back_propagation(output_value, true_value, weights):
+    cost = [(label[i] - output_value[i]) ** 2 for i in range(2)]
+    for curr_neuron in Network.Hidden3.neurons:
+        for prev_neuron in Network.Hidden2.neurons:
+            return
+
+
+back_propagation(predictions, label, weight_matrix)
