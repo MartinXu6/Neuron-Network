6870b3a775a84cefc337827f452b7a65370fe0da
stecheiguess <aquaignix@gmail.com>
2024-01-30 19:20:18 +0000

    file change

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import random
 from PIL import Image
 
 # extracting Image RGB values
-image = Image.open(r"D:\pycharm projects\Neuron-Network\images\cat1.jpg")
+image = Image.open(r"images/cat1.jpg")
 label = [0, 1]
 image.thumbnail((200, 200))
 pixels = image.load()
@@ -45,7 +45,8 @@ class Neuron_layer:
         self.input_neurons = input_neurons
         self.neuron_num = neuron_num
         self.weights = weights
-        self.neurons = [Neuron(self.input_neurons, self.weights[i]) for i in range(self.neuron_num)]
+        self.neurons = [Neuron(self.input_neurons, self.weights[i])
+                        for i in range(self.neuron_num)]
 
     def Softmax(self, outputs):
         divider = sum([math.e ** i for i in outputs])
@@ -64,9 +65,12 @@ class Neural_Network:
     def __init__(self, picture):
         self.picture = picture
         self.input_layer = Neuron_layer(self.picture, 50, weight_matrix[0])
-        self.Hidden1 = Neuron_layer(self.input_layer.get_output(), 50, weight_matrix[1])
-        self.Hidden2 = Neuron_layer(self.Hidden1.get_output(), 50, weight_matrix[2])
-        self.Hidden3 = Neuron_layer(self.Hidden2.get_output(), 2, weight_matrix[3])
+        self.Hidden1 = Neuron_layer(
+            self.input_layer.get_output(), 50, weight_matrix[1])
+        self.Hidden2 = Neuron_layer(
+            self.Hidden1.get_output(), 50, weight_matrix[2])
+        self.Hidden3 = Neuron_layer(
+            self.Hidden2.get_output(), 2, weight_matrix[3])
         self.out = self.Hidden3.get_output()
 
 
